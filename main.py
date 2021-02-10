b79c21b4a35831f5f5ac1822899bef7ce4b106fe
andrewcgaitskell <andrew@gaitskell.com>
2021-02-10 08:33:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 # pygame.init()
+## https://www.reddit.com/r/pygame/comments/e9h35x/disable_audio_engine/
 pygame.display.init()
 pygame.font.init()
 # pygame.mixer.init()  # disable sound
